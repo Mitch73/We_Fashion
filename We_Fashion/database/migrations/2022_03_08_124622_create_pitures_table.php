16fcfe5482e03811cfa6ac8686acdf8ba9780b9f
mich <djackson2284@gmail.com>
2022-03-08 15:06:40 +0100

    First_Commit

---
+++ We_Fashion/database/migrations/2022_03_08_124622_create_pitures_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pitures', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->string('link', 100);
+            $table->string('title', 100)->nullable();
+            //SI ON SUPRIMME UN BOOK ALORS on suprimme les images associées
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade'); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pitures');
+    }
+};
